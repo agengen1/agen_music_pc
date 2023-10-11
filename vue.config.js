543e05cda175d82084a6e711dddfa8c5a690b8d0
namegen <2073923271@qq.com>
2023-10-12 00:55:06 +0800

    fix:修复代码打包，游览器暴露源码问题

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  //防止代码泄露
+  productionSourceMap: false,
+  configureWebpack: (config) => {
+    config.devtool = false;
+  },
+
   publicPath: process.env.VUE_APP_BASE_URL,
 });
